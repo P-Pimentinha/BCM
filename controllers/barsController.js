71eae538ef33cb1dfb12ef0d3ce782e897e18ee3
Pedro Pimentinha <pedro.pimentinha18@gmail.com>
2023-01-15 13:38:15 +0100

    adds delete bar// update bar route

--- controllers/barsController.js
+++ controllers/barsController.js
@@ -1,6 +1,7 @@
 import { StatusCodes } from 'http-status-codes';
 import Bar from '../models/Bars.js';
 import { BadRequestError, NotFoundError } from '../errors/index.js';
+import checkPermissions from '../utils/checkPermission.js';
 
 const createBar = async (req, res) => {
   const { name } = req.body;
@@ -16,7 +17,18 @@ const createBar = async (req, res) => {
 };
 
 const deleteBar = async (req, res) => {
-  res.send('delete');
+  const { id: barId } = req.params;
+
+  const bar = await Bar.findByIdAndDelete({ _id: barId });
+
+  if (!bar) {
+    throw new NotFoundError(`No Bar with ID ${barId}`);
+  }
+
+  checkPermissions(req.user, bar.createdBy);
+
+  await bar.remove();
+  res.status(StatusCodes.OK).json({ msg: 'Success! Bar removed' });
 };
 
 const getAllBars = async (req, res) => {
@@ -31,7 +43,27 @@ const showStats = async (req, res) => {
 };
 
 const updateBar = async (req, res) => {
-  res.send('update');
+  const { id: barId } = req.params;
+  const { name } = req.body;
+
+  if (!name) {
+    throw new BadRequestError('Please Provide All Values');
+  }
+
+  const bar = await Bar.findOne({ _id: barId });
+
+  if (!bar) {
+    throw new NotFoundError(`No bar with id ${barId}`);
+  }
+
+  checkPermissions(req.user, bar.createdBy);
+
+  const updatedBar = await Bar.findByIdAndUpdate({ _id: barId }, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(StatusCodes.OK).json({ updatedBar });
 };
 
 export { createBar, deleteBar, getAllBars, updateBar, showStats };
